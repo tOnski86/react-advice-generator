fabeff5ff92143e7d9bb3376ddc5f30678307ddf
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-14 18:57:38 +0800

    Implement data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import mobileDividerIcon from '/pattern-divider-mobile.svg';
 import desktopDividerIcon from '/pattern-divider-desktop.svg';
 import diceIcon from '/icon-dice.svg';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [{ id, advice }, setAdvice] = useState({});
+  const [generateAdvice, setGenerateAdvice] = useState(false);
+
+  function handleGenerateAdvice() {
+    setGenerateAdvice(!generateAdvice);
+  }
+
+  useEffect(() => {
+    async function getAdvice() {
+      const res = await fetch('https://api.adviceslip.com/advice');
+      const data = await res.json();
+
+      const { id, advice } = data.slip;
+      setAdvice({ id, advice });
+    }
+    getAdvice();
+  }, [generateAdvice]);
+
   return (
-    <div className='bg-grey px-6 sm:px-8 text-center mx-4 rounded-lg flex flex-col items-center pt-10 sm:max-w-md'>
-      <h2 className='text-green text-[11px] font-medium uppercase tracking-[0.3em] mb-6'>
-        Advice #117
+    <div className='bg-grey px-6 sm:px-8 text-center mx-4 rounded-lg flex flex-col items-center pt-10 sm:max-w-lg'>
+      <h2 className='text-green text-[11px] sm:text-xs font-medium uppercase tracking-[0.3em] mb-6'>
+        Advice #{id}
       </h2>
       <blockquote>
-        <p className='text-white text-2xl font-bold'>
-          &ldquo;It is easy to sit up and take notice, what&apos;s difficult is
-          getting up and taking action.&rdquo;
+        <p className='text-white text-2xl sm:text-[28px] sm:leading-[2.2rem] font-bold'>
+          &ldquo;{advice}&rdquo;
         </p>
       </blockquote>
       <img
@@ -22,10 +41,13 @@ function App() {
       <img
         src={desktopDividerIcon}
         alt='desktop divider'
-        className='hidden sm:block mb-12 mt-6'
+        className='hidden sm:block mb-12 mt-8'
       />
 
-      <button className='relative after:bg-green after:absolute after:-translate-x-1/2 after:-translate-y-1/2 after:top-1/2 after:left-1/2 after:h-full after:w-full after:-z-10 z-10 after:p-8 after:rounded-full -mb-3 hover:drop-shadow-glowBig active:drop-shadow-glowSmall transition-all ease-in-out active:scale-95 [&>*]:hover:animate-spin'>
+      <button
+        className='relative after:bg-green after:absolute after:-translate-x-1/2 after:-translate-y-1/2 after:top-1/2 after:left-1/2 after:h-full after:w-full after:-z-10 z-10 after:p-8 after:rounded-full -mb-3 hover:drop-shadow-glowBig active:drop-shadow-glowSmall transition-all ease-in-out active:scale-95 [&>*]:hover:animate-spin'
+        onClick={handleGenerateAdvice}
+      >
         <img src={diceIcon} alt='dice icon' />
       </button>
     </div>
